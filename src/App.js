e1ba42a30d87e6b7bec41a10068854fe0a7f7e88
erindj <erindjasini@hotmail.com>
2018-12-09 13:30:40 -0800

    creating a Scroll component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -21,13 +27,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+
+    if (this.state.robots.length === 0 ){
+        return <h1>Loading</h1>
+    } else {
     return (
     <div className = 'tc'>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
+        <Scroll>
         <CardList robots={filteredRobots} />
+        </Scroll>
     </div>
         );
+        }
     }
 }
 
